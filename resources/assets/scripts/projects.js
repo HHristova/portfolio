c08bda3bf11e5fdb80b67b7d7994a2f306144cea
Hristina Hristova <hristina.e.hristova@gmail.com>
2019-05-10 22:09:35 +0300

    Projects improvements

--- resources/assets/scripts/projects.js
+++ resources/assets/scripts/projects.js
@@ -1,58 +1,205 @@
 [
     {
         "id": "1",
-        "image": "https://cdn.dribbble.com/users/926161/screenshots/5592430/dribbble212_4x.jpg",
-        "alt": "Landing page design 1",
-        "title": "Landing page design 1",
-        "description": "featured"
+        "image": "public/assets/images/myWork/web-design/tenant-app-login.jpg",
+        "class": "col-12@xs col-8@sm col-6@md",
+        "alt": "Tenant app login page design",
+        "filterId": "webdesign"
     },
     {
         "id": "2",
-        "image": "https://cdn.dribbble.com/users/926161/screenshots/5546260/dribbble3_4x.jpg",
-        "alt": "Landing page design 2",
-        "title": "Landing page design 2",
-        "description": "business"
+        "image": "public/assets/images/myWork/web-design/eqology-website-concept-v1.jpg",
+        "class": "col-12@xs col-4@sm col-6@md",
+        "alt": "Website concept for scandinavian company",
+        "filterId": "webdesign"
     },
     {
         "id": "3",
-        "image": "https://cdn.dribbble.com/users/926161/screenshots/5534075/untitled-1_4x.jpg",
-        "alt": "Landing page design 3",
-        "title": "Landing page design 3",
-        "description": "fit"
+        "image": "public/assets/images/myWork/illustrations/lettering-1.jpg",
+        "class": "col-12@xs col-4@sm col-6@md",
+        "alt": "Hand-lettering design",
+        "filterId": "illustration"
     },
     {
         "id": "4",
-        "image": "https://cdn.dribbble.com/users/926161/screenshots/5592430/dribbble212_4x.jpg",
-        "alt": "Landing page design 4",
-        "title": "Landing page design 4",
-        "description": "lifeBalance"
+        "image": "public/assets/images/myWork/web-design/gotenant-landing-page-design.jpg",
+        "class": "col-12@xs col-4@sm col-6@md",
+        "alt": "Landing page design of property management platform in UK",
+        "filterId": "webdesign"
     },
     {
         "id": "5",
-        "image": "https://cdn.dribbble.com/users/926161/screenshots/5546260/dribbble3_4x.jpg",
-        "alt": "Landing page design 5",
-        "title": "Landing page design 5",
-        "description": "animalCare"
+        "image": "public/assets/images/myWork/web-design/gotenant-features-page-design.jpg",
+        "class": "col-12@xs col-8@sm col-6@md",
+        "alt": "Features page design",
+        "filterId": "webdesign"
     },
     {
         "id": "6",
-        "image": "https://cdn.dribbble.com/users/926161/screenshots/5546260/dribbble3_4x.jpg",
-        "alt": "Landing page design 6",
-        "title": "Landing page design 6",
-        "description": "beautyWellness"
+        "image": "public/assets/images/myWork/web-design/eqology-website-concept-v2.jpg",
+        "class": "col-12@xs col-4@sm col-3@md",
+        "alt": "Website concept for scandinavian company",
+        "filterId": "webdesign"
     },
     {
         "id": "7",
-        "image": "https://cdn.dribbble.com/users/926161/screenshots/5546260/dribbble3_4x.jpg",
-        "alt": "Landing page design 7",
-        "title": "Landing page design 7",
-        "description": "packs"
+        "image": "public/assets/images/myWork/illustrations/lettering-2.jpg",
+        "class": "col-12@xs col-4@sm col-3@md",
+        "alt": "Hand-lettering idea",
+        "filterId": "illustration"
     },
     {
         "id": "8",
-        "image": "https://cdn.dribbble.com/users/926161/screenshots/5546260/dribbble3_4x.jpg",
-        "alt": "Landing page design 8",
-        "title": "Landing page design 8",
-        "description": "sonya"
+        "image": "public/assets/images/myWork/illustrations/lettering-4.jpg",
+        "class": "col-12@xs col-4@sm col-3@md",
+        "alt": "Hand-lettering design",
+        "filterId": "illustration"
+    },
+    {
+        "id": "9",
+        "image": "public/assets/images/myWork/illustrations/illustration-balloons.jpg",
+        "class": "col-12@xs col-4@sm col-3@md",
+        "alt": "Flying balloons illustration based on a real image",
+        "filterId": "illustration"
+    },
+    {
+        "id": "10",
+        "image": "public/assets/images/myWork/illustrations/lettering-17.jpg",
+        "class": "col-12@xs col-4@sm col-3@md",
+        "alt": "Spring inspired hand-lettered J",
+        "filterId": "illustration"
+    },
+    {
+        "id": "11",
+        "image": "public/assets/images/myWork/illustrations/lettering-15.jpg",
+        "class": "col-12@xs col-8@sm col-6@md",
+        "alt": "Hello April hand-lettering",
+        "filterId": "illustration"
+    },
+    {
+        "id": "12",
+        "image": "public/assets/images/myWork/web-design/psp-webtech-site-design.jpg",
+        "class": "col-12@xs col-4@sm col-6@md",
+        "alt": "Website design of PSP Webtech company",
+        "filterId": "webdesign"
+    },
+    {
+        "id": "13",
+        "image": "public/assets/images/myWork/illustrations/christmas-party-PSP.jpg",
+        "class": "col-12@xs col-4@sm col-3@md",
+        "alt": "Christmas party poster design",
+        "filterId": "illustration"
+    },
+    {
+        "id": "14",
+        "image": "public/assets/images/myWork/illustrations/lettering-7.jpg",
+        "class": "col-12@xs col-4@sm col-3@md",
+        "alt": "Hand-lettering idea",
+        "filterId": "illustration"
+    },
+    {
+        "id": "15",
+        "image": "public/assets/images/myWork/illustrations/lettering-14.jpg",
+        "class": "col-12@xs col-4@sm",
+        "alt": "Hand-lettered H",
+        "filterId": "illustration"
+    },
+    {
+        "id": "16",
+        "image": "public/assets/images/myWork/illustrations/lettering-3.jpg",
+        "class": "col-12@xs col-4@sm col-3@md",
+        "alt": "Hand-lettering idea",
+        "filterId": "illustration"
+    },
+    {
+        "id": "17",
+        "image": "public/assets/images/myWork/illustrations/lettering-16.jpg",
+        "class": "col-12@xs col-4@sm col-3@md",
+        "alt": "Birthday card design",
+        "filterId": "illustration"
+    },
+    {
+        "id": "18",
+        "image": "public/assets/images/myWork/web-design/GoTenant-project.jpg",
+        "class": "col-12@xs col-4@sm col-6@md",
+        "alt": "Website dashboard design",
+        "filterId": "webdesign"
+    },
+    {
+        "id": "19",
+        "image": "public/assets/images/myWork/illustrations/spring-illustration.jpg",
+        "class": "col-12@xs col-8@sm col-6@md",
+        "alt": "Spring illustration",
+        "filterId": "illustration"
+    },
+    {
+        "id": "20",
+        "image": "public/assets/images/myWork/web-design/WhatsOn-app-design.png",
+        "class": "col-12@xs col-4@sm col-6@md",
+        "alt": "Web application design",
+        "filterId": "webdesign"
+    },
+    {
+        "id": "21",
+        "image": "public/assets/images/myWork/web-design/personal-website-design-frontend.jpg",
+        "class": "col-12@xs col-4@sm col-6@md",
+        "alt": "Personal website front end design",
+        "filterId": "webdesign"
+    },
+    {
+        "id": "22",
+        "image": "public/assets/images/myWork/web-design/rentsuccess-website-concept.jpg",
+        "class": "col-12@xs col-4@sm col-3@md",
+        "alt": "Property management platform landing page design concept",
+        "filterId": "webdesign"
+    },
+    {
+        "id": "23",
+        "image": "public/assets/images/myWork/web-design/subscription-mockup-v2.jpg",
+        "class": "col-12@xs col-4@sm col-3@md",
+        "alt": "Subscription page design",
+        "filterId": "webdesign"
+    },
+    {
+        "id": "24",
+        "image": "public/assets/images/myWork/illustrations/galaxy.jpg",
+        "class": "col-12@xs col-4@sm col-3@md",
+        "alt": "Simplified galaxy illustration",
+        "filterId": "illustration"
+    },
+    {
+        "id": "25",
+        "image": "public/assets/images/myWork/illustrations/rock-poster-design.jpg",
+        "class": "col-12@xs col-4@sm col-6@md",
+        "alt": "Christmas rock poster design concept",
+        "filterId": "illustration"
+    },
+    {
+        "id": "26",
+        "image": "public/assets/images/myWork/illustrations/lettering-9.jpg",
+        "class": "col-12@xs col-4@sm col-3@md",
+        "alt": "Valentine card design concept",
+        "filterId": "illustration"
+    },
+    {
+        "id": "27",
+        "image": "public/assets/images/myWork/illustrations/lettering-6.jpg",
+        "class": "col-12@xs col-4@sm col-3@md",
+        "alt": "Hand-lettering design",
+        "filterId": "illustration"
+    },
+    {
+        "id": "28",
+        "image": "public/assets/images/myWork/illustrations/lettering-13.jpg",
+        "class": "col-12@xs col-4@sm col-6@md",
+        "alt": "Hand-lettered word 'Evening'",
+        "filterId": "illustration"
+    },
+    {
+        "id": "29",
+        "image": "public/assets/images/myWork/illustrations/lettering-12.jpg",
+        "class": "col-12@xs col-4@sm col-6@md",
+        "alt": "Hand-lettered chocolate design",
+        "filterId": "illustration"
     }
 ]
