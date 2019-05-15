d937ad93bcae89087e90634086e71fa39abeb129
Hristina Hristova <hristina.e.hristova@gmail.com>
2019-05-15 21:30:02 +0300

    Projects page - fixed image filtering

--- resources/assets/scripts/index.js
+++ resources/assets/scripts/index.js
@@ -17,25 +17,20 @@ function checkHeader() {
     // Detect scroll position
     let scrollPosition = Math.round(window.scrollY);
 
-    // If scrolled 100px, add "sticky" class to the header
+    // If scrolled 100px, add 'sticky' class to the header
     if (scrollPosition > 100){
         document.querySelector('header').classList.add('sticky');
     }
-    // If not, remove "sticky" class from header
+    // If not, remove 'sticky' class from header
     else {
         document.querySelector('header').classList.remove('sticky');
     }
 };
 
-// Image replacement handler
-$(document).on("click", ".js-button", function() {
-    var imageAlt = $(this).parents(".picture-item__inner").find("img").attr("alt");
-    var imageSrc = $(this).parents(".picture-item__inner").find("img").attr("src");
-    $(".js-download").attr("href", imageSrc);
-    $(".js-modal-image").attr("src", imageSrc);
-    $(".js-modal-image").attr("alt", imageAlt);
-
-    $(document).on("click", ".js-heart", function() {
-        $(this).toggleClass("active");
-    });
+// Image gallery handler
+$(document).on('click', '.js-button', function() {
+    var imageAlt = $(this).parents('.picture-item__inner').find('img').attr('alt');
+    var imageSrc = $(this).parents('.picture-item__inner').find('img').attr('src');
+    $('.js-modal-image').attr('src', imageSrc);
+    $('.js-modal-image').attr('alt', imageAlt);
 });
