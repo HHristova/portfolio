a29da595aebd77915605384e9595ae57f399e144
Todor Nikolov <nikolov.todor@gmail.com>
2020-03-29 17:18:04 +0300

    Get blog resource content from api request.

--- resources/assets/views/pages/BlogInner.js
+++ resources/assets/views/pages/BlogInner.js
@@ -1,51 +1,55 @@
-import BlogPostsData from '../../scripts/blogposts.json';
-import Utils from '../../scripts/services/Utils.js'
+'use strict';
+
+import Utils from '../../scripts/services/Utils.js';
+import * as RequestService from '../../scripts/services/RequestService.js';
 
 let BlogInner = {
     render : async () => {
         let request = Utils.parseRequestURL();
 
-        let BlogPostsFiltered = BlogPostsData.filter(blogPost => blogPost.id === request.id);
-
-        let blogPost =  /*html*/`
-            ${ BlogPostsFiltered
-                .map(blogPost =>
-                /*html*/`
-                <div class="IllustrationProject-wrapper mt-bg">
-                    <div class="item__content px-2 px-md-3">
-                        <div class="max-chars blog-content">
-                            <h2 class="section__container__title section__container__title--sm m-neg
-                                d-inline-block px-3">
-                                <span class="section__container__title__span">
-                                    ${blogPost.title}
-                                </span>
-                            </h2>
-                            <img src="${blogPost.mainImage}" alt="${blogPost.title}" class="img-fluid" />
-                            <small class="my-2 d-block"><i>${blogPost.mainImageCredit}</i></small>
-                        </div>
-                `).join('\n ')
-            }
-        `;
-
-        let blogContentLength = Object.keys(BlogPostsFiltered[0].content).length;
+        let blogPost = '';
         let blogContent = '';
-        for(var i = 0; i < blogContentLength; i++) {
-            blogContent += `
-            <div class="max-chars blog-content">
-                ${BlogPostsFiltered[0].content['p'+i]}
-            </div>
+
+        await RequestService.getBlogIndex().then((response) => {
+            let BlogPostsFilteredApi = response.data.filter(blogPost => blogPost.id === request.id);
+
+            blogPost =  /*html*/`
+                ${ BlogPostsFilteredApi
+                    .map(blogPost =>
+                        /*html*/`
+                    <div class="IllustrationProject-wrapper mt-bg">
+                        <div class="item__content px-2 px-md-3">
+                            <div class="max-chars blog-content">
+                                <h2 class="section__container__title section__container__title--sm m-neg
+                                    d-inline-block px-3">
+                                    <span class="section__container__title__span">
+                                        ${blogPost.title}
+                                    </span>
+                                </h2>
+                                <img src="${blogPost.mainImage}" alt="${blogPost.title}" class="img-fluid" />
+                                <small class="my-2 d-block"><i>${blogPost.mainImageCredit}</i></small>
+                            </div>
+                    `).join('\n ')
+                }
             `;
-        }
-        blogContent += `
-            <div class="pin-it max-chars blog-content">
-                <a href="http://pinterest.com/pin/create/button/?url=https%3A%2F%2Fhhristova.com%2Fblog%2F${BlogPostsFiltered[0].id}&media=${BlogPostsFiltered[0].pinImageEncoded}&description=${BlogPostsFiltered[0].title}"
-                    class="pin-it-button custom" count-layout="horizontal">
-                </a>
-                <img src="${BlogPostsFiltered[0].pinImage}" class="img-fluid pin-background" alt="${BlogPostsFiltered[0].title}" />
-            </div>
-            </div>
-        </div>
-        `
+
+            let blogContentLength = Object.keys(BlogPostsFilteredApi[0].content).length;
+            for(var i = 0; i < blogContentLength; i++) {
+                blogContent += `
+                    <div class="max-chars blog-content">${BlogPostsFilteredApi[0].content['p'+i]}</div>
+                `;
+            }
+            blogContent += `
+                    <div class="pin-it max-chars blog-content">
+                        <a href="http://pinterest.com/pin/create/button/?url=https%3A%2F%2Fhhristova.com%2Fblog%2F${BlogPostsFilteredApi[0].id}&media=${BlogPostsFilteredApi[0].pinImageEncoded}&description=${BlogPostsFilteredApi[0].title}"
+                            class="pin-it-button custom" count-layout="horizontal">
+                        </a>
+                        <img src="${BlogPostsFilteredApi[0].pinImage}" class="img-fluid pin-background" alt="${BlogPostsFilteredApi[0].title}" />
+                    </div>
+                    </div>
+                </div>
+            `;
+        });
 
         let modals =  /*html*/`
             <div id="modalPicture" class="modal fade pr-0" tabindex="-1"
@@ -69,9 +73,9 @@ let BlogInner = {
                 </div>
             </div>
         </div>
-        `
+        `;
 
-        return blogPost + blogContent + modals
+        return blogPost + blogContent + modals;
     },
     after_render: async () => {
 
@@ -81,6 +85,6 @@ let BlogInner = {
             $('.js-modal-image').attr('src', imageSrc);
         });
     }
-}
+};
 
 export default BlogInner;
