931ae0244c94072ca1f9a93a4b2a76966b342485
HHristova <hristina.a.hristova@gmail.com>
2021-11-06 14:57:27 +0200

    Cookie modal added

--- resources/assets/views/components/Footer.js
+++ resources/assets/views/components/Footer.js
@@ -4,6 +4,9 @@ import ScrollReveal from 'scrollreveal'
 let Footer = {
     render: async () => {
         let view =  /*html*/`
+        <div class="cookie-consent bg-white shadow" id="cookie-consent"><span data-v-93de06f6="" class="cookie-consent__close text-black-light" id="cookie-close"></span>
+        <span data-v-93de06f6="" class="text-10 font-medium tracking-2p2 leading-1 uppercase text-black-light d-block mb-2">I use cookies &amp; I hope it's ok 😊</span> <p data-v-93de06f6="" class="text-body text-grey-darkest mb-2">This website uses cookies to ensure you get the selected season experience all over the website.</p> <span data-v-93de06f6="" class="button -hollow w-full block cursor-pointer">Got it!</span></div>
+
         <div class="section text-center container pt-0">
             <div class="section__container">
 
