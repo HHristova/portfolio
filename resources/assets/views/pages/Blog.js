d5162cfea2c6d9a78ef9206256b25ee63570d426
HHristova <hristina.a.hristova@gmail.com>
2022-01-10 23:48:42 +0200

    Blog page is back to business :D

--- resources/assets/views/pages/Blog.js
+++ resources/assets/views/pages/Blog.js
@@ -1,51 +1,51 @@
-'use strict';
-
-import * as RequestService from '../../scripts/services/RequestService.js';
+import BlogPostsData from '../../scripts/blogposts.json';
 
 let Blog = {
     render : async () => {
         let title =  /*html*/`
-            <div class="main-header">
-                <div class="hero-img hero-img--blog"></div>
+        <div class="px-4 mt-bg">
+            <div class="section__container text-center">
+                <h1 class="section__container__title text-center d-inline-block px-3">
+                    <span class="section__container__title__span">
+                        Blog
+                    </span>
+                </h1>
             </div>
-
-        `;
+        </div>
+        `
 
         let blogPosts = `
             <div class="mb-3 d-flex container flex-wrap justify-content-between blog-flex">
         `;
 
-        await RequestService.getBlogIndex().then((response) => {
-            for (let i = response.data.length; i--;) {
-                blogPosts += `
-                <div class="blog-item d-flex align-items-center mb-3">
-                     <a href="/blog/${response.data[i].id}"
-                         class="box-thumb box-thumb--blog d-inline-block"
-                         style="background-image: url('${response.data[i].thumb}');">
+        for(var i = BlogPostsData.length; i--;) {
+            blogPosts += `
+            <div class="blog-item d-flex align-items-center mb-3">
+                 <a href="/blog/${BlogPostsData[i].id}"
+                     class="box-thumb box-thumb--blog d-inline-block"
+                     style="background-image: url('${BlogPostsData[i].thumb}');">
+                 </a>
+                 <div class="box-content p-4">
+                     <a href="/blog/${BlogPostsData[i].id}">
+                         <div class="blog-title pt-2">
+                             ${BlogPostsData[i].title}
+                         </div>
                      </a>
-                     <div class="box-content p-4">
-                         <a href="/blog/${response.data[i].id}">
-                             <div class="blog-title pt-2">
-                                 ${response.data[i].title}
-                             </div>
-                         </a>
-                         <small class="blog-date">${response.data[i].date}</small>
-                         <span class="d-block blog-summary">
-                             ${response.data[i].summary}
-                         </span>
-                     </div>
-                </div>
-                `;
-            }
-        });
+                     <span class="d-block blog-summary">
+                         ${BlogPostsData[i].summary}
+                     </span>
+                 </div>
+              </div>
+            `;
+        }
 
         blogPosts += `
             </div>
-        `;
+        `
 
         return title + blogPosts
     },
     after_render: async () => {}
-};
+}
 
 export default Blog;
--- resources/assets/views/pages/BlogInner.js
+++ resources/assets/views/pages/BlogInner.js
@@ -1,55 +1,51 @@
-'use strict';
-
-import Utils from '../../scripts/services/Utils.js';
-import * as RequestService from '../../scripts/services/RequestService.js';
+import BlogPostsData from '../../scripts/blogposts.json';
+import Utils from '../../scripts/services/Utils.js'
 
 let BlogInner = {
     render : async () => {
         let request = Utils.parseRequestURL();
 
-        let blogPost = '';
-        let blogContent = '';
+        let BlogPostsFiltered = BlogPostsData.filter(blogPost => blogPost.id === request.id);
 
-        await RequestService.getBlogIndex().then((response) => {
-            let BlogPostsFilteredApi = response.data.filter(blogPost => blogPost.id === request.id);
-
-            blogPost =  /*html*/`
-                ${ BlogPostsFilteredApi
-                    .map(blogPost =>
-                        /*html*/`
-                    <div class="IllustrationProject-wrapper mt-bg">
-                        <div class="item__content px-2 px-md-3">
-                            <div class="max-chars blog-content">
-                                <h2 class="section__container__title section__container__title--sm m-neg
-                                    d-inline-block px-3">
-                                    <span class="section__container__title__span">
-                                        ${blogPost.title}
-                                    </span>
-                                </h2>
-                                <img src="${blogPost.mainImage}" alt="${blogPost.title}" class="img-fluid" />
-                                <small class="my-2 d-block"><i>${blogPost.mainImageCredit}</i></small>
-                            </div>
-                    `).join('\n ')
-                }
-            `;
-
-            let blogContentLength = Object.keys(BlogPostsFilteredApi[0].content).length;
-            for(var i = 0; i < blogContentLength; i++) {
-                blogContent += `
-                    <div class="max-chars blog-content">${BlogPostsFilteredApi[0].content['p'+i]}</div>
-                `;
+        let blogPost =  /*html*/`
+            ${ BlogPostsFiltered
+                .map(blogPost =>
+                /*html*/`
+                <div class="IllustrationProject-wrapper mt-bg">
+                    <div class="item__content px-2 px-md-3">
+                        <div class="max-chars blog-content">
+                            <h2 class="section__container__title section__container__title--sm m-neg
+                                d-inline-block px-3">
+                                <span class="section__container__title__span">
+                                    ${blogPost.title}
+                                </span>
+                            </h2>
+                            <img src="${blogPost.mainImage}" alt="${blogPost.title}" class="img-fluid" />
+                            <small class="my-2 d-block"><i>${blogPost.mainImageCredit}</i></small>
+                        </div>
+                `).join('\n ')
             }
+        `;
+
+        let blogContentLength = Object.keys(BlogPostsFiltered[0].content).length;
+        let blogContent = '';
+        for(var i = 0; i < blogContentLength; i++) {
             blogContent += `
-                    <div class="pin-it max-chars blog-content">
-                        <a href="http://pinterest.com/pin/create/button/?url=https%3A%2F%2Fhhristova.com%2Fblog%2F${BlogPostsFilteredApi[0].id}&media=${BlogPostsFilteredApi[0].pinImageEncoded}&description=${BlogPostsFilteredApi[0].title}"
-                            class="pin-it-button custom" count-layout="horizontal">
-                        </a>
-                        <img src="${BlogPostsFilteredApi[0].pinImage}" class="img-fluid pin-background" alt="${BlogPostsFilteredApi[0].title}" />
-                    </div>
-                    </div>
-                </div>
+            <div class="max-chars blog-content">
+                ${BlogPostsFiltered[0].content['p'+i]}
+            </div>
             `;
-        });
+        }
+        blogContent += `
+            <div class="pin-it max-chars blog-content">
+                <a href="http://pinterest.com/pin/create/button/?url=https%3A%2F%2Fhhristova.com%2Fblog%2F${BlogPostsFiltered[0].id}&media=${BlogPostsFiltered[0].pinImageEncoded}&description=${BlogPostsFiltered[0].title}"
+                    class="pin-it-button custom" count-layout="horizontal">
+                </a>
+                <img src="${BlogPostsFiltered[0].pinImage}" class="img-fluid pin-background" alt="${BlogPostsFiltered[0].title}" />
+            </div>
+            </div>
+        </div>
+        `
 
         let modals =  /*html*/`
             <div id="modalPicture" class="modal fade pr-0" tabindex="-1"
@@ -59,7 +55,7 @@ let BlogInner = {
                       <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                           <img class="close__icon"
-                            src="/assets/images/close-icon.svg" alt="Close icon">
+                            src="/public/assets/images/close-icon.svg" alt="Close icon">
                           <span class="sr-only">Close</span>
                         </button>
                       </div>
@@ -73,9 +69,9 @@ let BlogInner = {
                 </div>
             </div>
         </div>
-        `;
+        `
 
-        return blogPost + blogContent + modals;
+        return blogPost + blogContent + modals
     },
     after_render: async () => {
 
@@ -85,6 +81,6 @@ let BlogInner = {
             $('.js-modal-image').attr('src', imageSrc);
         });
     }
-};
+}
 
 export default BlogInner;
